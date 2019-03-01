818fec6e97193fb2e6f61cf0606554ca81bbf497
Aaron Sutula <aaron.sutula@gmail.com>
2019-03-01 14:14:20 -0600

    Implemented ContactsApi

--- Textile/ContactsApi.h
+++ Textile/ContactsApi.h
@@ -8,9 +8,23 @@
 
 #import "ApiModule.h"
 
+@class Contact;
+@class ContactList;
+@class ThreadList;
+@class MobileSearchHandle;
+@class ContactQuery;
+@class QueryOptions;
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface ContactsApi : ApiModule
+
+- (void)addContact:(Contact *)contact error:(NSError **)error;
+- (Contact *)contact:(NSString *)contactId error:(NSError **)error;
+- (ContactList *)contacts:(NSError **)error;
+- (void)removeContact:(NSString *)contactId error:(NSError **)error;
+- (ThreadList *)contactThreads:(NSString *)contactId error:(NSError **)error;
+- (MobileSearchHandle *)searchContacts:(ContactQuery *)query options:(QueryOptions *)options error:(NSError **)error;
 
 @end
 
--- Textile/ContactsApi.m
+++ Textile/ContactsApi.m
@@ -7,7 +7,37 @@
 //
 
 #import "ContactsApi.h"
+#import <Mobile/Mobile.h>
+#import "../node_modules/@textile/go-mobile/dist/ios/protos/Model.pbobjc.h"
+#import "../node_modules/@textile/go-mobile/dist/ios/protos/Query.pbobjc.h"
 
 @implementation ContactsApi
+
+- (void)addContact:(Contact *)contact error:(NSError **)error {
+  [self.textile.node addContact:contact.data error:error];
+}
+
+- (Contact *)contact:(NSString *)contactId error:(NSError **)error {
+  NSData *data = [self.textile.node contact:contactId error:error];
+  return [[Contact alloc] initWithData:data error:error];
+}
+
+- (ContactList *)contacts:(NSError **)error {
+  NSData *data = [self.textile.node contacts:error];
+  return [[ContactList alloc] initWithData:data error:error];
+}
+
+- (void)removeContact:(NSString *)contactId error:(NSError **)error {
+  [self.textile.node removeContact:contactId error:error];
+}
+
+- (ThreadList *)contactThreads:(NSString *)contactId error:(NSError **)error {
+  NSData *data = [self.textile.node contactThreads:contactId error:error];
+  return [[ThreadList alloc] initWithData:data error:error];
+}
+
+- (MobileSearchHandle *)searchContacts:(ContactQuery *)query options:(QueryOptions *)options error:(NSError **)error {
+  return [self.textile.node searchContacts:query.data options:options.data error:error];
+}
 
 @end
