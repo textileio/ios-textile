7cb592758935232b9b72903be20f4478d99f4805
Aaron Sutula <aaron.sutula@gmail.com>
2019-05-06 20:46:23 -0500

    Switch order of Files.list params
    Signed-off-by: Aaron Sutula <aaron.sutula@gmail.com>

--- Textile/Classes/FilesApi.m
+++ Textile/Classes/FilesApi.m
@@ -69,7 +69,7 @@
   return [[Block alloc] initWithData:data error:error];
 }
 
-- (FilesList *)list:(NSString *)offset limit:(long)limit threadId:(NSString *)threadId error:(NSError * _Nullable __autoreleasing *)error {
+- (FilesList *)list:(NSString *)threadId offset:(NSString *)offset limit:(long)limit error:(NSError * _Nullable __autoreleasing *)error {
   NSData *data = [self.node files:offset != nil ? offset : @"" limit:limit threadId:threadId error:error];
   if (*error) {
     return nil;
