0332363c92750c45913cf7acaec51c2fd1a3f487
andrew <andrewxhill@gmail.com>
2019-05-08 15:58:42 -0700

    account: enable delete method
    Signed-off-by: andrew <andrewxhill@gmail.com>

--- Textile/Classes/AccountApi.m
+++ Textile/Classes/AccountApi.m
@@ -14,24 +14,28 @@
   return self.node.address;
 }
 
-- (NSString *)seed {
-  return self.node.seed;
-}
-
-- (NSData *)encrypt:(NSData *)data error:(NSError * _Nullable __autoreleasing *)error {
-  return [self.node encrypt:data error:error];
-}
-
-- (NSData *)decrypt:(NSData *)data error:(NSError * _Nullable __autoreleasing *)error {
-  return [self.node decrypt:data error:error];
-}
-
 - (Contact *)contact:(NSError * _Nullable __autoreleasing *)error {
   NSData *data = [self.node accountContact:error];
   if (*error) {
     return nil;
   }
   return [[Contact alloc] initWithData:data error:error];
+}
+
+- (NSData *)decrypt:(NSData *)data error:(NSError * _Nullable __autoreleasing *)error {
+  return [self.node decrypt:data error:error];
+}
+
+- (void)delete:(NSString *)repoPath address:(NSString *)address  error:(NSError * _Nullable __autoreleasing *)error {
+  [self.node deleteAccount:repoPath address:address error:error];
+}
+
+- (NSData *)encrypt:(NSData *)data error:(NSError * _Nullable __autoreleasing *)error {
+  return [self.node encrypt:data error:error];
+}
+
+- (NSString *)seed {
+  return self.node.seed;
 }
 
 - (MobileSearchHandle *)sync:(QueryOptions *)options error:(NSError * _Nullable __autoreleasing *)error {
