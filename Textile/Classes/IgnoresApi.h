cc1153b9542b676be082187de42c894b5da94b07
Aaron Sutula <hi@asutula.com>
2019-05-23 17:53:36 -0500

    Ignores docs
    Signed-off-by: Aaron Sutula <hi@asutula.com>

--- Textile/Classes/IgnoresApi.h
+++ Textile/Classes/IgnoresApi.h
@@ -10,8 +10,17 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ * Provides access to ignores related Textile APIs
+ */
 @interface IgnoresApi : NodeDependant
 
+/**
+ * Ignore data by block
+ * @param blockId The id of the block you want to ignore
+ * @param error A reference to an error pointer that will be set in the case of an error
+ * @return The block id of the newly created igore block
+ */
 - (NSString *)add:(NSString *)blockId error:(NSError **)error;
 
 @end
